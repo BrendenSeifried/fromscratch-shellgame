b05d7be248a87022e0ccaeafcb19fc39c0755e8c
BrendenSeifried <brendenseifried@gmail.com>
2022-01-24 16:53:48 -0800

    kind of working but the ball has a picture attached and its flying all over the place

--- app.js
+++ app.js
@@ -3,9 +3,9 @@ const shell1btn = document.getElementById('shell1-btn');
 const shell2btn = document.getElementById('shell2-btn');
 const shell3btn = document.getElementById('shell3-btn');
 
-const shellone = document.getElementById('shell-one');
-const shelltwo = document.getElementById('shell-two');
-const shellthree = document.getElementById('shell-three');
+const shellOne = document.getElementById('shell-one');
+const shellTwo = document.getElementById('shell-two');
+const shellThree = document.getElementById('shell-three');
 
 const totalEL = document.getElementById('total');
 const winsEL = document.getElementById('wins');
@@ -38,6 +38,33 @@ shell3btn.addEventListener('click', () => {
     const answer = hidingPlaces[hidingSpot];
     handleGuess(answer, 'three');
 });
+
   // get user input
+function handleGuess(correct, pick) {
+    shellOne.classList.remove('ball');
+    shellTwo.classList.remove('ball');
+    shellThree.classList.remove('ball');
+    //totalGuesses++;
+    //console.log(totalGuesses);
+
+    if (correct === 'one') {
+        shellOne.classList.add('ball');
+
+    } else if (correct === 'two') {
+        shellTwo.classList.add('ball');
+
+    } else if (correct === 'three') 
+        shellThree.classList.add('ball');
+    
+    if (pick === correct) {
+        //correctGuesses++;
+    }
+
+    /*totalEL.textContent = totalGuesses;
+    winsEL.textContent = correctGuesses;
+    lossesEL.textContent = totalGuesses - correctGuesses;*/
+
+    
+}
   // use user input to update state 
   // update DOM to reflect the new state
