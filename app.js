1d4c9d827e21b68e0a8450794443991550a787b2
BrendenSeifried <brendenseifried@gmail.com>
2022-01-24 16:16:38 -0800

    added const in javascript by referencing the other tree,shed,boulder game

--- app.js
+++ app.js
@@ -1,4 +1,17 @@
 // import functions and grab DOM elements
+const shell1btn = document.getElementById('shell1-btn');
+const shell2btn = document.getElementById('shell2-btn');
+const shell3btn = document.getElementById('shell3-btn');
+
+const shellone = document.getElementById('shell-one');
+const shelltwo = document.getElementById('shell-two');
+const shellthree = document.getElementById('shell-three');
+
+const totalEL = document.getElementById('total');
+const winsEL = document.getElementById('wins');
+const lossesEL = document.getElementById('losses');
+
+const hidingPlaces = ['one', 'two', 'three'];
 
 // let state
 
